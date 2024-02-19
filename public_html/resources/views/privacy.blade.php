1f675d6f786bf562b2b455c3b2323906dc34c4a3
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2024-02-19 12:41:30 +0100

    FIX | in PIPELINE SALES FORCE

--- public_html/resources/views/privacy.blade.php
+++ public_html/resources/views/privacy.blade.php
@@ -202,7 +202,7 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                PROMEDYA Sales Force
+                PROMEDYA | Sales Force
                 <small>&nbsp;&nbsp;<b id="countdown"></b></small>
             </h1>
             <br>
