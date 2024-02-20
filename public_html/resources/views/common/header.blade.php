e5a43f2d1e98acd78bb307cf7f03476e4ea18ff4
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2024-02-20 10:06:19 +0100

    Fix Bug Grafico

--- public_html/resources/views/common/header.blade.php
+++ public_html/resources/views/common/header.blade.php
@@ -20,7 +20,6 @@
     <link rel="stylesheet"
           href="<?php echo URL::asset('backend/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') ?>">
     <link rel="stylesheet" href="<?php echo URL::asset('backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 
     <link rel="stylesheet" href="<?php echo URL::asset('backend/dist/css/adminlte.min.css') ?>">
@@ -36,7 +35,6 @@
           href="<?php echo URL::asset('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') ?>">
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
     <link type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.6/nv.d3.min.js">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="<?php echo URL::asset('datetimepicker/css/bootstrap-datetimepicker.css') ?>">
     <link rel="stylesheet"
           href="<?php echo URL::asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') ?>">
