30773aa9c5e2071c31b2bb530db3e960209712f9
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2024-03-21 16:21:56 +0100

    Tutti possono modificare la gestione invio mail

--- public_html/resources/views/mail.blade.php
+++ public_html/resources/views/mail.blade.php
@@ -1,58 +1,55 @@
 <?php $utente = session('utente'); ?>
 @include('common.header')
 <div class="content-wrapper">
-    @if ($utente->username != 'Giovanni Tutino')
-        <div style="display: flex;justify-content: center;align-items: center;padding-top: 5%;">
-            <img alt="WORK IN PROGRESS" style="min-height: 25vh;min-width: 25vw;"
-                 src="https://www.b-fast.it/wp-content/uploads/2021/08/come-correggere-errore-siamo-spiacenti-non-sei-autorizzato-ad-accedere-a-questa-pagina-in-wordpress.jpg">
-        </div>
-    @else
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1 style="color:#007bff">
-                PROMEDYA | Sales Force
-                <small>&nbsp;&nbsp;<b id="countdown"></b></small>
-            </h1>
-            <br>
-        </section>
-        <!-- Main content -->
-        <section class="content" style="margin:5%;">
-            <table id="example3" class="table table-bordered datatable">
-                <thead>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1 style="color:#007bff">
+            PROMEDYA | Sales Force
+            <small>&nbsp;&nbsp;<b id="countdown"></b></small>
+        </h1>
+        <br>
+    </section>
+    <!-- Main content -->
+    <section class="content" style="margin:5%;">
+        <table id="example3" class="table table-bordered datatable">
+            <thead>
+            <tr>
+                <th class="no-sort">Id</th>
+                <th class="no-sort">Valore</th>
+                <th class="no-sort">Azioni</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($mail as $m)
                 <tr>
-                    <th class="no-sort">Id</th>
-                    <th class="no-sort">Valore</th>
-                    <th class="no-sort">Azioni</th>
+                    <td>{{ $m->id}}</td>
+                    <td>@if($m->valore == 1)
+                            Mail Attive
+                        @endif @if($m->valore == 0)
+                            Mail Disattivate
+                        @endif</td>
+                    <form enctype="multipart/form-data" method="post"
+                          onsubmit="return confirm('Sei sicuro di voler eliminare la riga selezionata?')">
+                        @csrf
+                        <td class="no-sort"
+                            style="background:white;border-width:1px">
+                            <div style="display:flex;gap: 2px;">
+                                <button type="button" onclick="modifica(<?php echo $m->id;?>)"
+                                        class="form-control btn-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
+                                         fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                        <path
+                                            d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </td>
+                    </form>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach($mail as $m)
-                    <tr>
-                        <td>{{ $m->id}}</td>
-                        <td>@if($m->valore == 1) Mail Attive @endif @if($m->valore == 0) Mail Disattivate @endif</td>
-                        <form enctype="multipart/form-data" method="post"
-                              onsubmit="return confirm('Sei sicuro di voler eliminare la riga selezionata?')">
-                            @csrf
-                            <td class="no-sort"
-                                style="background:white;border-width:1px">
-                                <div style="display:flex;gap: 2px;">
-                                    <button type="button" onclick="modifica(<?php echo $m->id;?>)"
-                                            class="form-control btn-primary">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"
-                                             fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                                            <path
-                                                d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                                        </svg>
-                                    </button>
-                                </div>
-                            </td>
-                        </form>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </section>
-    @endif
+            @endforeach
+            </tbody>
+        </table>
+    </section>
 </div>
 <!-- /.container-fluid-->
 
